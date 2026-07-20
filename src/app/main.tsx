8386f62946fd9e85181a92243e13393a99a3c968
Arteluff <mastertema2005@gmail.com>
2026-07-20 15:31:50 +0300

    feat: add base api layout

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { Provider } from "react-redux";
+import { store } from "./store.ts";
 
 async function enableMocking() {
   if (import.meta.env.MODE !== "development") {
@@ -17,7 +19,9 @@ async function enableMocking() {
 enableMocking().then(() => {
   createRoot(document.getElementById("root")!).render(
     <StrictMode>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </StrictMode>,
   );
 });
---
+++ src/app/store.ts
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { baseApi } from "../shared/api/baseApi";
+import { authSlice } from "../entities/user/authSlice";
+
+export const store = configureStore({
+  reducer: {
+    [baseApi.reducerPath]: baseApi.reducer,
+    auth: authSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(baseApi.middleware),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
